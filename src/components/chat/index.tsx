05ce7944e844818fc14691cf5f39f803b0a7a455
bilkn <bilkan.konus@gmail.com>
2021-06-20 18:31:56 +0300

    feat: add chat component

---
+++ src/components/chat/index.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import {
+  Container,
+  MessageBox,
+  Text,
+  TextInput,
+  Date,
+  Button,
+} from "./styles/chat";
+import { iProps } from "../../ts/interfaces";
+
+function Chat({ children, ...rest }: iProps) {
+  return <Container {...rest}> {children}</Container>;
+}
+
+Chat.MessageBox = function ChatMessageBox({ children, ...rest }: iProps) {
+  return <MessageBox {...rest}>{children}</MessageBox>;
+};
+
+Chat.Text = function ChatText({ children, ...rest }: iProps) {
+  return <Text {...rest}>{children}</Text>;
+};
+
+Chat.TextInput = function ChatTextInput({ ...rest }: iProps) {
+  return <TextInput {...rest} />;
+};
+
+Chat.Date = function ChatDate({ children, ...rest }: iProps) {
+  return <Date {...rest}>{children}</Date>;
+};
+
+Chat.Button = function ChatButton({ children, ...rest }: iProps) {
+  return <Button {...rest}>{children}</Button>;
+};
+
+export default Chat;
---
+++ src/components/chat/styles/chat.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+export const Container = styled.div`` 
+
+export const MessageBox = styled.div`` 
+
+export const Text = styled.p`` 
+
+export const TextInput = styled.input`` 
+
+export const Date = styled.div``
+
+export const Button = styled.button`` 
